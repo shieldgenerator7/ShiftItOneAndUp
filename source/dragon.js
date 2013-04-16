837bbfe62485f700c667d502fde6a4f634b24212
BrutalBrainiac <richard.conley@otterbein.edu>
2013-04-16 13:54:15 -0300

    Update dragon.js

--- source/dragon.js
+++ source/dragon.js
@@ -1,2 +1,34 @@
-//main.js
-clear();
+//dragon.js
+
+var tank function() {
+var speed = 1;
+var health = 50;
+var damage = 20;
+
+
+}
+
+var basic function() {
+var speed = 3;
+var health = 10;
+var damage = 5;
+
+
+}
+
+var speedy function() {
+var speed = 5;
+var health = 5;
+var damage = 10;
+
+
+}
+
+var range function() {
+var speed = 1;
+var health = 10;
+var damage = 10;
+
+
+}
+
