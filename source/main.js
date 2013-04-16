9066ac1b4fb362385b4d80b71ea44c972249a399
BrutalBrainiac <richard.conley@otterbein.edu>
2013-04-16 13:17:13 -0300

    Update main.js

--- source/main.js
+++ source/main.js
@@ -1,8 +1,10 @@
 //main.js
-var width = 320, 
-	height = 500,
+var width = 800, 
+	height = 480,
 	c = document.getElementById('c'), 
 	ctx = c.getContext('2d');
+	var img = document.getElementById("gamebackground");
 	c.width = width;
 	c.height = height;
+	ctx.drawImage(img,0,0);
 clear();
