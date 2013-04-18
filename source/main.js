cd6219018d744fb04184404c9118b895348da99f
Shield Generator <shieldgenerator7@yahoo.com>
2013-04-18 11:41:10 -0400

    Made basic changes to main.js
    Put in main game loop.
    Put calls to your draw function in the main method

--- source/main.js
+++ source/main.js
@@ -1,10 +1,19 @@
 //main.js
+
 var width = 800, 
-	height = 480,
-	c = document.getElementById('c'), 
-	ctx = c.getContext('2d');
+	height = 480;
+var c = document.getElementById('c');
+var ctx = c.getContext('2d');
 	var img = document.getElementById("gamebackground");
 	c.width = width;
 	c.height = height;
+
+var main = function(){
+
 	ctx.drawImage(img,0,0);
-clear();
+	
+	mLoop = setTimeout(main, 1000 / 50);
+}
+
+main();
+
