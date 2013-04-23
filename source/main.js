8bc5aad2656b7ef0dcc6d604f85173b70864c92b
corewth <bsmucker@gmail.com>
2013-04-23 13:00:55 -0300

    Added code for railgun sound on click

--- source/main.js
+++ source/main.js
@@ -299,6 +299,36 @@ function Fire(x, y, Dx, Dy){
 var howManyFire = 0;
 var fireArray = [];
 
+//Brad: begin code for firing sound, taken from http://www.javascriptkit.com/script/script2/soundlink.shtml
+var html5_audiotypes={
+	"wav": "audio/wav"
+}
+
+function createsoundbite(sound){
+	var html5audio=document.createElement('audio')
+	if (html5audio.canPlayType){ //check support for HTML5 audio
+		for (var i=0; i<arguments.length; i++){
+			var sourceel=document.createElement('source')
+			sourceel.setAttribute('src', arguments[i])
+			if (arguments[i].match(/\.(\w+)$/i))
+				sourceel.setAttribute('type', html5_audiotypes[RegExp.$1])
+			html5audio.appendChild(sourceel)
+		}
+		html5audio.load()
+		html5audio.playclip=function(){
+			html5audio.pause()
+			html5audio.currentTime=0
+			html5audio.play()
+		}
+		return html5audio
+	}
+	else{
+		return {playclip:function(){throw new Error("Your browser doesn't support HTML5 audio. Try using Google Chrome!")}}
+	}
+}
+var railgunsound=createsoundbite("railgun_sound.wav")
+//Brad: end code for railgun sound
+
 
 var player = new (function(){
 	var that = this;
@@ -375,6 +405,7 @@ document.addEventListener('mousemove', function(e){
 
 document.addEventListener('mousedown', function(e){
 		player.fire();
+		railgunsound.playclip(); //Brad: code to play railgun sound
 });
 
 
